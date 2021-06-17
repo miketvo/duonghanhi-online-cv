2ced3ed5de10d05264c9d7966c736589216f4bba
miketvo <ikxplicitie@gmail.com>
2021-06-17 18:37:48 +0700

    Update copyrights + added Webstorm project configuration files.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2021 Mike Vo (info@miketvo.com) - All Rights Reserved.
+ *
+ * This file is part of the project "duonghanhi-online-cv".
+ *
+ * This project is an online curriculum vitae authored by Mike Vo - hereby
+ * referred to as "the author" - for Ms. Duong Ha Nhi - hereby referred to as
+ * "the client".
+ *
+ * COPYING AND/OR DISTRIBUTING OF ANY CONTENT WITHIN THIS PROJECT WITHOUT EX-
+ * PRESS PERMISSION FROM THE CLIENT AND/OR THE AUTHOR, VIA ANY MEDIUM IS
+ * STRICTLY PROHIBITED.
+ *
+ * Written by Mike Vo on June 17th 2021.
+ */
+
 import * as React from "react"
 
 import Layout from "../components/layout"
