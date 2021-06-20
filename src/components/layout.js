db5e2a11c891c335d5d6de1e101c3b0c6f750cbd
miketvo <ikxplicitie@gmail.com>
2021-06-21 03:14:53 +0700

    Removed unused code

--- src/components/layout.js
+++ src/components/layout.js
@@ -22,7 +22,6 @@
 import * as React from "react";
 import PropTypes from "prop-types";
 import { useStaticQuery, graphql } from "gatsby";
-import { StaticImage } from "gatsby-plugin-image";
 
 import Header from "./header";
 import MobileHamburgerMenu from "./mobileHamburgerMenu";
